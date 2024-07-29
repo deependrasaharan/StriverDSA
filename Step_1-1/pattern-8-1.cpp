a178ce49997d02daa47e49198b0104b67a8d69d1
deependrasaharan <saharandeependra@gmail.com>
2024-07-29 18:15:24 +0530

    Simple general code

---
+++ Step_1-1/pattern-8-1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int n;
+  cout<<"Enter number of rows: ";
+  cin>>n;
+  for(int i=n;i>=1;i--)
+  {
+    for(int j=n-1;j>=i;j--)
+    {
+      cout<<' ';
+    }
+    for(int k=2*i-1;k>=1;k--)
+    {
+      cout<<'*';
+    }
+    cout<<'\n';
+  }
+
+  return 0;
+}
